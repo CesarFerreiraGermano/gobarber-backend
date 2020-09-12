2ce319e45cfc577a9494563a4b40d3886ce030c5
Cesar Germano <cesar.ferreira.germano@gmail.com>
2020-09-12 00:10:03 -0300

    Last classroom

--- src/routes/users.routes.ts
+++ src/routes/users.routes.ts
@@ -1,8 +1,12 @@
 import { Router } from 'express';
+import multer from 'multer';
 
 import CreateUserService from '../services/CreateUserService';
+import ensureAuthenticated from '../middlewares/ensureAuthenticated'
+import uploadConfig from '../config/upload';
 
 const usersRouter = Router();
+const upload = multer(uploadConfig);
 
 usersRouter.post('/', async (request, response) => {
     try {
@@ -22,6 +26,10 @@ usersRouter.post('/', async (request, response) => {
     } catch(err) {
         return response.status(400).json({error: err.message});
     }
+});
+
+usersRouter.patch('/avatar', ensureAuthenticated, upload.single('avatar'), async (request, response) => {
+    return response.json({ message: 'Its working' });
 });
 
 export default usersRouter;
